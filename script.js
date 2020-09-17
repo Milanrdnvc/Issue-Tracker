d21b9cdf055c9f8d3a68acebd1331b56dcb930bc
Milanrdnvc <milanradinovic@rocketmail.com>
2020-09-17 20:43:30 +0200

    Implemented add issue btn

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const tabBtns = Array.from(document.querySelectorAll('.btn--tab'));
 const addBtn = document.querySelector('.btn--add');
 const addIssueTab = document.querySelector('.add-issue');
 const currentIssuesTab = document.querySelector('.current-issues');
+const descInput = document.querySelector('.add-issue__input');
+const assignToInput = document.querySelectorAll('.add-issue__input')[1];
+const priorityInput = document.querySelectorAll('.add-issue__input')[2];
 
 function switchTabs(e) {
-  if (tabBtns.indexOf(e.target) === 0) {
+  if (tabBtns.indexOf(e.target) === 0 || e === 'switchAfterCreation') {
     addIssueTab.classList.remove('active');
     currentIssuesTab.classList.add('active');
     tabBtns[0].classList.add('active');
@@ -17,6 +20,38 @@ function switchTabs(e) {
   }
 }
 
+function addIssue() {
+  const forbiddenStrings = ['Assign Issue To...', 'Select priority...'];
+  if (
+    descInput.value !== '' &&
+    assignToInput.value !== '' &&
+    assignToInput.value !== forbiddenStrings[0] &&
+    priorityInput !== '' &&
+    priorityInput !== forbiddenStrings[1]
+  ) {
+    makeIssue(assignToInput.value, priorityInput.value, descInput.value);
+  }
+}
+
+function makeIssue(username, priority, desc) {
+  const issueMarkup = `
+    <div class="current-issues__info">
+      <b>Assigned</b>: ${username}
+      <span class="current-issues__close">Close Issue</span>
+    </div>
+    <div class="current-issues__info"><b>Priority</b>: ${priority}</div>
+    <div class="current-issues__info">
+      <b>Description:</b><br /><span class="current-issues__description">${desc}</span>
+    </div>`;
+  const currentIssue = document.createElement('div');
+  currentIssue.classList.add('current-issues__issue');
+  currentIssue.innerHTML = issueMarkup;
+  currentIssuesTab.appendChild(currentIssue);
+  switchTabs('switchAfterCreation');
+}
+
 tabBtns.forEach((btn) => {
   btn.addEventListener('click', switchTabs);
 });
+
+addBtn.addEventListener('click', addIssue);
