efe47fd6700a709f7b0e5127ad305aa03a48dc29
Milanrdnvc <milanradinovic@rocketmail.com>
2020-09-17 19:24:12 +0200

    Fixed "Close Issue" btn

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const addBtn = document.querySelector('.btn--add');
 const addIssueTab = document.querySelector('.add-issue');
 const currentIssuesTab = document.querySelector('.current-issues');
 
+function switchTabs(e) {
+  if (tabBtns.indexOf(e.target) === 0) {
+    addIssueTab.classList.remove('active');
+    currentIssuesTab.classList.add('active');
+    tabBtns[0].classList.add('active');
+    tabBtns[1].classList.remove('active');
+  } else {
+    currentIssuesTab.classList.remove('active');
+    addIssueTab.classList.add('active');
+    tabBtns[0].classList.remove('active');
+    tabBtns[1].classList.add('active');
+  }
+}
+
 tabBtns.forEach((btn) => {
-  btn.addEventListener('click', (e) => {
-    if (tabBtns.indexOf(e.target) === 0) {
-      addIssueTab.classList.remove('active');
-      currentIssuesTab.classList.add('active');
-      tabBtns[0].classList.add('active');
-      tabBtns[1].classList.remove('active');
-    } else {
-      currentIssuesTab.classList.remove('active');
-      addIssueTab.classList.add('active');
-      tabBtns[0].classList.remove('active');
-      tabBtns[1].classList.add('active');
-    }
-  });
+  btn.addEventListener('click', switchTabs);
 });
